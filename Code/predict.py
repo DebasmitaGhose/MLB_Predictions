e2fa1dc2521f8e1a2e80ac66be061103a2559b2b
DebasmitaGhose <debasmita2pat@gmail.com>
2019-12-07 16:07:22 -0500

    Replaced missing values with mean value in the column

--- Code/predict.py
+++ Code/predict.py
@@ -6,17 +6,16 @@ from sklearn.datasets import make_classification
 import numpy as np
 import pandas as pd
 import collections
+from sklearn.impute import SimpleImputer
+
 X = []
 
 with open('../../Data/temp_mlb_predictions.csv') as csvfile:
     mlb_row = csv.reader(csvfile, delimiter=',')
     for row in mlb_row:
-        #print(type(row))
         X.append(row)
-        #print ', '.join(row)
 
 X = X[1:] # remove the first row as it is the header     
-# print(X[0])
 X_arr = np.array(X) # convert to a numpy array
 
 y = X_arr[:,1] # 2nd column is the label
@@ -27,17 +26,14 @@ X_arr[X_arr == 'NA'] = -100 # convert NA to nan
 
 X_arr = X_arr.astype(float)
 
-print(np.nan)
 X_arr[X_arr == -100] = np.nan # convert NA to nan
 
-row = X_arr[0]
-print(X_arr[np.isnan(X_arr)].shape)
 
-
-
+#Remove the columns from the feature set if the percentange of nan values are more than 50%
 X_select = []
 select_col = []
 threshold = 50
+print("Column IDs with percentage of missing values")
 for i in range(X_arr.shape[1]):
     col_i = X_arr[:,i]
     num_nan_in_col = col_i[np.isnan(col_i)].shape[0]
@@ -49,10 +45,25 @@ for i in range(X_arr.shape[1]):
         select_col.append(i)
         X_select.append(col_i)
 
-
 X_select = np.array(X_select)
 X_select = np.transpose(X_select)
-print(X_select.shape)
 
 
-                     
+#Replace missing values with mean of the column
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp.fit(X_select)
+X_without_nan = imp.transform(X_select)
+
+##### SANITY CHECK --> to check the replacement of missing values #########
+#print(np.where(np.isnan(X_select[:,10])==True))
+#print(X_select[55])
+#print(X_without_nan[55])
+
+
+
+
+
+
+
+
+                  
