c086beeba759e670c94557b31875219670118fc4
DebasmitaGhose <debasmita2pat@gmail.com>
2019-12-10 01:07:05 -0500

    added code to oversample the data and plot confusion matrix

--- Code/predict.py
+++ Code/predict.py
@@ -5,10 +5,52 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import precision_recall_fscore_support
+from imblearn.over_sampling import SMOTE 
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.metrics import confusion_matrix
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score,classification_report,confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
 
 import numpy as np
 import csv
+import itertools
 
+
+def plot_confusion_matrix(cm, classes=[0,1],
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    # if normalize:
+    #     cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    #     print("Normalized confusion matrix")
+    # else:
+    #     print('Confusion matrix, without normalization')
+ 
+    print (cm)
+ 
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+ 
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+ 
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 X_train = []
 X_test = []
@@ -101,7 +143,20 @@ X_without_nan_test = imp.transform(X_select_test)
 print("Size of the Testing set:", X_without_nan_test.shape)
 
 
-X_train = X_without_nan_train
+########## OVERSAMPLING ##############
+ros = RandomOverSampler(random_state=None, sampling_strategy='minority')
+X_train, y_train = ros.fit_resample(X_without_nan_train, y_train)
+#X_train_over_sampled, y_train_over_sampled = ros.fit_resample(X_without_nan_train, y_train)
+#X_test, y_test = ros.fit_resample(X_without_nan_test, y_test)
+
+
+########### UNDERSAMPLING #############
+#rus = RandomUnderSampler(random_state=42, sampling_strategy='majority')
+#X_train, y_train = rus.fit_resample(X_train_over_sampled, y_train_over_sampled)
+
+print(y_train.shape,'after over_sampling')
+
+#X_train = X_without_nan_train
 X_test = X_without_nan_test
 
 ##### SANITY CHECK --> to check the replacement of missing values #########
@@ -115,26 +170,37 @@ X_test = X_without_nan_test
 #print('Number of training examples',X_train.shape)
 #print('Number of test samples',X_test.shape)
 
-# Logistic Regressor Classifier
+# Logistic Regressor Cimport itertoolslassifier
 print("LOGISTIC REGRESSION")
-clf = LogisticRegression(random_state=0).fit(X_train, y_train)
-Y_predict = clf.predict(X_test)
-print("Predictions", Y_predict)
+clf = LogisticRegression(random_state=None).fit(X_train, y_train)
+Y_predict_LR = clf.predict(X_test)
+print("Predictions", Y_predict_LR)
 print("Classification Accuracy = ", clf.score(X_test, y_test))
-print("Precision, Recall, F-Score:", precision_recall_fscore_support(y_test, Y_predict, average='weighted'))
+print("Precision, Recall, F-Score:", precision_recall_fscore_support(y_test, Y_predict_LR, average='weighted'))
 
+'''
 # Support Vector Machine(SVM) Classifier
 print("SUPPORT VECTOR MACHINE")
 clf = SVC(gamma='auto').fit(X_train, y_train)
-Y_predict = clf.predict(X_test)
-print("Predictions", Y_predict)
+Y_predict_SVM = clf.predict(X_test)
+print("Predictions", Y_predict_SVM)
 print("Classification Accuracy = ", clf.score(X_test, y_test))
-print("Precision, Recall, F-Score:", precision_recall_fscore_support(y_test, Y_predict, average='weighted'))
+print("Precision, Recall, F-Score:", precision_recall_fscore_support(y_test, Y_predict_SVM, average='weighted'))
+'''
+
+c = confusion_matrix(y_test, Y_predict_LR, labels=[0,1])
+#print(c)
+
+class_names = ["No Hall of Fame", "Hall of Fame"]
+
+plot_confusion_matrix(c)
+
+plt.show()
 
 #print(Y_predict.type)
 filename = 'predictions.csv'
 
-Y_predict_list = Y_predict.tolist()
+Y_predict_list = Y_predict_LR.tolist()
 print(len(Y_predict_list))
 
 # writing to csv file 
@@ -142,7 +208,7 @@ with open(filename, 'w') as csvfile:
     # creating a csv writer object 
     csvwriter = csv.writer(csvfile) 
     for i in zip(Y_predict_list):
-		csvwriter.writerow((i))
+        csvwriter.writerow((i))
 
     # writing the data rows 
     #csvwriter.writerows(Y_predict)
